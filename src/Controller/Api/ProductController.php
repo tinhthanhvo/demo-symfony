5ed1ef92707e41bec04aba890025e581077c5097
tanthanhpham <tanthanh.pham@nfq.asia>
2022-03-18 15:22:27 +0700

    Create API insert product

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -6,8 +6,10 @@ use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use App\Service\FileUploader;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -46,18 +48,23 @@ class ProductController extends AbstractFOSRestController
     /**
      * @Rest\Post("/products")
      * @param Request $request
-     *
+     * @param FileUploader $fileUploader
      * @return Response
      */
-    public function insertProduct(Request $request): Response
+    public function insertProduct(Request $request, FileUploader $fileUploader): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->submit($request->request->all());
         if ($form->isSubmitted() && $form->isValid()) {
+            $uploadFile = $request->files->get('image');
+            if ($uploadFile) {
+                $saveFile = $fileUploader->upload($uploadFile);
+                $product->setImage($saveFile);
+            }
             $this->productRepository->add($product);
 
-            return $this->handleView($this->view(null, Response::HTTP_CREATED));
+            return $this->handleView($this->view($product, Response::HTTP_CREATED));
         }
 
         return $this->handleView($this->view($form->getErrors()));
